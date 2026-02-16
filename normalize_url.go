8ecef3fbbb50dae2d548573f8b13e0237a88f540
Utkarsh736 <utkarshtomar736@gmail.com>
2026-02-16 18:12:11 +0530

    Add normalizeURL function with comprehensive tests

---
+++ normalize_url.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+func normalizeURL(inputURL string) (string, error) {
+	parsedURL, err := url.Parse(inputURL)
+	if err != nil {
+		return "", fmt.Errorf("couldn't parse URL: %w", err)
+	}
+
+	fullPath := parsedURL.Host + parsedURL.Path
+	fullPath = strings.ToLower(fullPath)
+	fullPath = strings.TrimRight(fullPath, "/") 
+
+	return fullPath, nil
+}
