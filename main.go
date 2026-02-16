a630b1d39d1413b2c93bf99285a1c7dd81ef9cb4
Utkarsh736 <utkarshtomar736@gmail.com>
2026-02-16 19:00:44 +0530

    Add CLI argument validation and start message

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-}
+	// Get command line arguments (excluding program name)
+	args := os.Args[1:]
 
+	// Validate number of arguments
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	// Extract the base URL
+	baseURL := args[0]
+
+	// Print start message
+	fmt.Printf("starting crawl of: %s\n", baseURL)
+}
